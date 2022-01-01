012529ea9a5124464cd068200e893a351e8076fa
m-ohamed <mohamed.adel-ibrahim@student.guc.edu.eg>
2022-01-01 11:52:37 +0200

    updating customer entity to add phone number validation

--- src/main/java/com/phonebook/repository/entity/CustomerEntity.java
+++ src/main/java/com/phonebook/repository/entity/CustomerEntity.java
@@ -4,9 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.regex.Pattern;
+
+import static com.phonebook.common.Constants.*;
 
 @Data
 @NoArgsConstructor
@@ -17,4 +18,15 @@ public class CustomerEntity {
     private Integer id;
     private String name;
     private String phone;
+
+    @Transient
+    private boolean state;
+
+    @PostLoad
+    private void postLoad() {
+        if(Pattern.matches(CAMEROON_NUMBER_REGEX + "|" + ETHIOPIA_NUMBER_REGEX + "|"
+                + MOROCCO_NUMBER_REGEX + "|" + MOZAMBIQUE_NUMBER_REGEX + "|" + UGANDA_NUMBER_REGEX, this.phone)) {
+            this.state = true;
+        }
+    }
 }
